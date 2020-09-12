978379ab10d0b32f1d5eb683206d77fa528d4400
Lay Mui <mui_ume@hotmail.com>
2020-09-12 21:39:36 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React , { useState} from 'react';
 import { StyleSheet, Text, View, TextInput, Button , ScrollView , FlatList } from 'react-native';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
+import { testProperties } from './Utils/testHelper';
 
 
 export default function App() {
