91925faa147ee302d08d663d4bac1f4ee7030950
LayMui <laymui.toh@taiger.com>
2020-10-24 16:55:57 +0800

    Build ipa artefact

---
+++ ios/WebDriverAgentRunner/WebDriverAgentRunner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebDriverAgentRunner
+//
+//  Created by Lay mui Toh on 15/9/20.
+//  Copyright © 2020 Lay mui Toh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
