a6b60fe729c3cf8e66b9de3645662aa7ffdcca04
LayMui <laymui.toh@taiger.com>
2020-10-24 17:01:09 +0800

    Build iOS artefact

---
+++ Utils/testHelper.js
@@ -0,0 +1,23 @@
+/**
+ * Add a unique test id for iOS and Android
+ *
+ * @param {string} id
+ * @param {boolean} disableAccessible All touchable elements are accessible, meaning that it
+ * groups its children into a single selectable component, sometimes this is not needed for testing
+ *
+ * @return {object}
+ */
+
+ /* Method 1 */
+export function testProperties(id, disableAccessible = false) {
+    const disableAccessibility = disableAccessible ? { accessible: false } : {};
+  
+    return { ...disableAccessibility, accessibilityLabel: id };
+  }
+
+/* Method 2 */
+/*
+  export function testProperties(id) {
+    return {testID: id, accessibilityLabel: id};
+  }
+  */
